96b24cbc26ccfff8bc912fb114770785ce24aceb
Vishakha Krishnarao Jadhav <132731497+vishakha-jadhav-02@users.noreply.github.com>
2023-07-29 14:11:53 +0530

    Create Program02.c
    Program to print 5 times Jay Ganesh on console

---
+++ Simple Problem Statements/Program02.c
@@ -0,0 +1,20 @@
+// Print 5 times Jay Ganesh on screen
+
+#include<stdio.h>
+
+void Display()
+{
+  int i = 0;
+
+  for(i =1; i<= 5; i++)
+    {
+      printf("Jay Ganesh...\n");
+    }
+}
+
+int main()
+{
+  Display();
+  
+  return 0;
+}
