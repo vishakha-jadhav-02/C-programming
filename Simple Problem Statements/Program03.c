444269853459a03257af611f8e99b96cad80467d
Vishakha Krishnarao Jadhav <132731497+vishakha-jadhav-02@users.noreply.github.com>
2023-07-29 19:41:11 +0530

    Create Program03.c
    Program to print 5 to 1 numbers on console

---
+++ Simple Problem Statements/Program03.c
@@ -0,0 +1,19 @@
+// Program to print 5 to 1 numbers on console
+
+#include<stdio.h>
+
+void Display()
+{
+  int i = 0;
+  for(i = 5; i > 0; i--)
+    {
+      printf("%d",i);
+    }
+}
+
+int main()
+{
+  Display();
+
+  return 0;
+}
