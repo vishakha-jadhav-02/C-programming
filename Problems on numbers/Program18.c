bd9e8f4aceb92dbdbcf76bb547a531ac79c9ca6f
Vishakha Krishnarao Jadhav <132731497+vishakha-jadhav-02@users.noreply.github.com>
2023-07-30 19:14:21 +0530

    Create Program18.c
    Accept number from user and check whether number is perfect or not

---
+++ Problems on numbers/Program18.c
@@ -0,0 +1,80 @@
+// Accept number from user and check whether number is perfect or not
+
+#include<stdio.h>
+#include<stdbool.h>
+
+/////////////////////////////////////////////////////
+//
+// Function Name:   CheckDivisible
+// Input:           Integer
+// Output:          Integer
+// Description:     return summation of factors 
+// Author:          Vishakha Krishnarao Jadhav
+// Date:            30/07/23
+//
+////////////////////////////////////////////////////
+int SumFactors(int iNo)
+{
+    int iCnt = 0;
+    int iSum = 0;
+
+    if(iNo < 0)     // Updator
+    {
+        iNo = -iNo;
+    }
+
+    for(iCnt = 1; iCnt <= (iNo/2); iCnt++)
+    {
+        if((iNo % iCnt) == 0)
+        {
+            iSum = iSum + iCnt;
+        }
+    }
+    return iSum;
+}
+
+/////////////////////////////////////////////////////
+//
+// Function Name:   CheckDivisible
+// Input:           Integer
+// Output:          Boolean
+// Description:     check whether input is perfect number 
+//
+////////////////////////////////////////////////////
+bool CheckPerfect(int iNumber)
+{
+    int iResult = 0;
+
+    iResult = SumFactors(iNumber);
+    if(iResult == iNumber)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+// Entry point function 
+
+int main()
+{
+    int iValue = 0;
+    bool bRet = false;
+
+    printf("Enter number : \n");
+    scanf("%d",&iValue);
+
+    bRet = CheckPerfect(iValue);
+    
+    if(bRet == true)
+    {
+        printf("%d is perfect number\n",iValue);
+    }
+    else
+    {
+        printf("%d is not a perfect number\n",iValue);
+    }
+    return 0;
+}
