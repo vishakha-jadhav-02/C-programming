a7a5fd795921207136855d4194c7e67317dd680d
Vishakha Krishnarao Jadhav <132731497+vishakha-jadhav-02@users.noreply.github.com>
2023-07-30 19:17:37 +0530

    Create Program19.c
    Accept number from user and check whether number is perfect or not

---
+++ Problems on numbers/Program19.c
@@ -0,0 +1,65 @@
+// Accept number from user and check whether number is perfect or not
+
+#include<stdio.h>
+#include<stdbool.h>
+
+/////////////////////////////////////////////////////
+//
+// Function Name:   CheckPerfect
+// Input:           Integer
+// Output:          Boolean
+// Description:     check whether input is perfect number or not
+// Author:          Vishakha Krishnarao Jadhav
+// Date:            30/07/23
+//
+////////////////////////////////////////////////////
+bool CheckPerfect(int iNo)
+{
+    int iCnt = 0;
+    int iSum = 0;
+
+    for(iCnt = 1; iCnt <= (iNo/2) ; iCnt++)
+    {
+        if((iNo % iCnt) == 0)
+        {
+            iSum = iSum + iCnt;
+        }
+        if(iSum > iNo)
+        {
+            break;
+        }
+    }
+
+    if(iSum == iNo)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+///////////////////////////////////////////////////////////////
+// Entry point function
+///////////////////////////////////////////////////////////////
+int main()
+{
+    int iValue = 0;
+    bool bRet = false;
+
+    printf("Enter number : \n");
+    scanf("%d",&iValue);
+
+    bRet = CheckPerfect(iValue);
+
+    if(bRet == true)
+    {
+        printf("%d is a perfect number\n",iValue);
+    }
+    else
+    {
+        printf("%d is not a perfect number\n",iValue);        
+    }
+    return 0;
+}
