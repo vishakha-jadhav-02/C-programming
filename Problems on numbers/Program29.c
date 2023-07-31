6f1107cd6330f73bd51edb75ac2d0a9861f03975
Vishakha Krishnarao Jadhav <132731497+vishakha-jadhav-02@users.noreply.github.com>
2023-07-31 17:10:15 +0530

    Create Program29.c
    Accept N numbers from user and return average of that N numbers

---
+++ Problems on numbers/Program29.c
@@ -0,0 +1,60 @@
+// Accept N numbers from user and return average of that N numbers
+
+#include<stdio.h>
+#include<stdlib.h>
+  
+/////////////////////////////////////////////////////
+//
+// Function Name:   AvgNumbers
+// Input:           Integer array, Integer
+// Output:          Float
+// Description:     return average of numbers
+// Author:          Vishakha Krishnarao Jadhav
+// Date:            31/07/23
+//
+////////////////////////////////////////////////////
+float AvgNumbers(int Arr[], int iLength)
+{
+  int iCnt = 0, iSum = 0;
+
+  for(iCnt = 0; iCnt < iLength; iCnt++)
+  {
+    iSum = iSum + Arr[iCnt];
+  }
+
+  avg = iSum / iLength;
+
+  return avg;
+}
+
+////////////////////////////////////////////////////
+// Entry point function
+///////////////////////////////////////////////////
+int main()
+{
+  int iSize = 0, iCnt = 0;
+  int *ptr = NULL;
+  float fRet = 0.0;
+
+  printf("Enter number of elements: ");
+  scanf("%d", &iSize);
+
+  ptr = (int*)malloc(iSize * sizeof(int));
+
+  printf("Enter the elements: \n");
+  for(iCnt = 0; iCnt < iSize; iCnt++)
+  {
+    scanf("%d", &ptr[iCnt]);
+  }
+
+  printf("Elements of array: \n");
+  for(iCnt = 0; iCnt < iSize; iCnt++)
+  {
+    printf("%d", ptr[iCnt]);
+  }
+
+  fRet = AvgNumbers(ptr, iSize);
+  printf("Average of numbers is: %f",iRet);
+
+  return 0;
+}
