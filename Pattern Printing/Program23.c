920b0d00b59d736a248f0b2a85da3957cfb4ba82
Vishakha Krishnarao Jadhav <132731497+vishakha-jadhav-02@users.noreply.github.com>
2023-08-02 18:56:02 +0530

    Create Program23.c
    Accept row and column count from user and display pattern
        Input: Row: 4    Col: 4
        Output:
        1   &   1   &
        2   &   2   &
        3   &   3   &
        4   &   4   &

---
+++ Pattern Printing/Program23.c
@@ -0,0 +1,60 @@
+/*
+Accept row and column count from user and display pattern
+    Input: Row: 4    Col: 4
+    Output:
+    1   &   1   &
+    2   &   2   &
+    3   &   3   &
+    4   &   4   &
+*/
+
+#include<stdio.h>
+
+/////////////////////////////////////////////////////
+//
+// Function Name:   Display
+// Input:           Integer
+// Output:          None
+// Description:     Display pattern
+// Author:          Vishakha Krishnarao Jadhav
+// Date:            02/08/23
+//
+////////////////////////////////////////////////////
+void Display(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if((j%2) != 0 )
+            {
+                printf("%d\t",i);
+            }
+            else
+            {
+                printf("&\t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+////////////////////////////////////////////////////
+// Entry point function
+////////////////////////////////////////////////////
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &iValue1);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &iValue2);
+
+    Display(iValue1, iValue2);
+
+    return 0;
+}
