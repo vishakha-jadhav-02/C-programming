078d1fd97a5f6b323a8d6e1bb7c6835b7ddafadc
Vishakha Krishnarao Jadhav <132731497+vishakha-jadhav-02@users.noreply.github.com>
2023-08-02 14:39:58 +0530

    Create Program06.c
    // Accept number from user and print that number of alphabets.
    // Input: 3
    // Output: a  b  c

---
+++ Pattern Printing/Program06.c
@@ -0,0 +1,43 @@
+// Accept number from user and print that number of alphabets.
+// Input: 3
+// Output: a  b  c
+
+#include<stdio.h>
+
+/////////////////////////////////////////////////////
+//
+// Function Name:   Display
+// Input:           Integer
+// Output:          None
+// Description:     Display alphabets
+// Author:          Vishakha Krishnarao Jadhav
+// Date:            02/08/23
+//
+////////////////////////////////////////////////////
+void Display(int iNo)
+{
+    int iCnt = 0;
+    char ch = 'a';
+
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf("%c\t", ch);
+        ch++;
+    }
+    printf("\n");
+}
+
+////////////////////////////////////////////////////
+// Entry point function
+////////////////////////////////////////////////////
+int main()
+{
+    int iFrequency = 0;
+
+    printf("Enter the frequency of symbol: ");
+    scanf("%d", &iFrequency);
+
+    Display(iFrequency);
+
+    return 0;
+}
