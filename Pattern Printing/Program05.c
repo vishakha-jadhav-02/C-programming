a22be799f259491a39e41913267fc33c64f80370
Vishakha Krishnarao Jadhav <132731497+vishakha-jadhav-02@users.noreply.github.com>
2023-08-02 14:32:58 +0530

    Create Program05.c
    // Accept number from user and display pattern like below
    // Input: 7
    // Output: 7    *    6  *   5   *   4   *   3   *   2   *   1   *

---
+++ Pattern Printing/Program05.c
@@ -0,0 +1,41 @@
+// Accept number from user and display pattern like below
+// Input: 7
+// Output: 7    *    6  *   5   *   4   *   3   *   2   *   1   *
+
+#include<stdio.h>
+
+/////////////////////////////////////////////////////
+//
+// Function Name:   Display
+// Input:           Integer
+// Output:          None
+// Description:     Display pattern 
+// Author:          Vishakha Krishnarao Jadhav
+// Date:            02/08/23
+//
+////////////////////////////////////////////////////
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = iNo; iCnt >= 1; iCnt--)
+    {
+        printf("%d\t*\t", iCnt);
+    }
+    printf("\n");
+}
+
+////////////////////////////////////////////////////
+// Entry point function
+////////////////////////////////////////////////////
+int main()
+{
+    int iFrequency = 0;
+
+    printf("Enter the frequency of symbol: ");
+    scanf("%d", &iFrequency);
+
+    Display(iFrequency);
+
+    return 0;
+}
