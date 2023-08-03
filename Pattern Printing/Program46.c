57d63c6edc91a2cb84df733988946add0bc2688e
Vishakha Krishnarao Jadhav <132731497+vishakha-jadhav-02@users.noreply.github.com>
2023-08-03 19:50:11 +0530

    Create Program46.c
    Accept row and column count from user and display pattern
    Input: Row: 4    Col: 4
    Output:
                    *   
                *   *
            *   *   *
        *   *   *   *

---
+++ Pattern Printing/Program46.c
@@ -0,0 +1,66 @@
+/*
+Accept row and column count from user and display pattern
+Input: Row: 4    Col: 4
+Output:
+                *   
+            *   *
+        *   *   *
+    *   *   *   *
+*/
+
+#include<stdio.h>
+
+/////////////////////////////////////////////////////
+//
+// Function Name:   Display
+// Input:           Integers
+// Output:          None
+// Description:     Display pattern
+// Author:          Vishakha Krishnarao Jadhav
+// Date:            03/08/23
+//
+////////////////////////////////////////////////////
+void Display(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    if(iRow != iCol)
+    {
+        printf("Invalid Input");
+        return;
+    }
+
+    for(i = iRow; i >= 1; i--)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if(i <= j)
+            {
+                printf("*\t");
+            }
+            else
+            {
+                printf(" \t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+////////////////////////////////////////////////////
+// Entry point function
+////////////////////////////////////////////////////
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &iValue1);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &iValue2);
+
+    Display(iValue1, iValue2);
+
+    return 0;
+}
