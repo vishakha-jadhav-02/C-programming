b897ea20bff851f0a198548c3ab22f9c84ca7551
Vishakha Krishnarao Jadhav <132731497+vishakha-jadhav-02@users.noreply.github.com>
2023-08-03 22:46:17 +0530

    Create Program15.c
    Accept the string and character from user and obtain the first occurence of that character in the string

---
+++ Problems on string/Program15.c
@@ -0,0 +1,60 @@
+// Accept the string and character from user and obtain the first occurance of that character in the string
+
+#include<stdio.h>
+#define ERR_NOTFOUND -1
+
+/////////////////////////////////////////////////////
+//
+// Function Name:   FirstOccurance
+// Input:           String, character
+// Output:          Integer
+// Description:     return first occurence of character in string
+// Author:          Vishakha Krishnarao Jadhav
+// Date:            03/08/23
+//
+////////////////////////////////////////////////////
+int FirstOccurance(char *str, char cValue)
+{
+    int iCnt = 1;
+    int iPos = ERR_NOTFOUND;
+
+    while(*str != '\0')
+    {
+        if(*str == cValue)
+        {
+            iPos = iCnt;
+            break;
+        }
+        str++;
+        iCnt++;
+    }
+    return iPos;
+}
+
+////////////////////////////////////////////////////
+// Entry point function
+////////////////////////////////////////////////////
+int main()
+{
+    char Arr[20];
+    char ch='\0';
+    int iRet = 0;
+
+    printf("Enter string: ");
+    scanf("%[^\n']s",Arr);
+
+    printf("Enter the character: ");
+    scanf(" %c",&ch);
+
+    iRet = FirstOccurance(Arr, ch);
+    if(iRet == ERR_NOTFOUND)
+    {
+        printf("There is no such character.");
+    }
+    else
+    {
+        printf("First occurance of character %c is at: %d",ch,iRet);
+    }
+
+    return 0;
+}
