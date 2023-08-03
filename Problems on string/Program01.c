ca7d7995798c6afb41460951e431b8580e0a8b7c
Vishakha Krishnarao Jadhav <132731497+vishakha-jadhav-02@users.noreply.github.com>
2023-08-03 20:01:11 +0530

    Create Program01.c
    Accept character from user and check whether character is capital or not

---
+++ Problems on string/Program01.c
@@ -0,0 +1,51 @@
+// Accept character from user and check whether character is capital or not
+
+#include<stdio.h>
+#include<stdbool.h>
+
+/////////////////////////////////////////////////////
+//
+// Function Name:   Display
+// Input:           Character
+// Output:          Boolean
+// Description:     Check whether input character is capital or not
+// Author:          Vishakha Krishnarao Jadhav
+// Date:            03/08/23
+//
+////////////////////////////////////////////////////
+bool CheckCapital(char cValue)
+{
+    if((cValue >= 65) && (cValue <= 90))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+////////////////////////////////////////////////////
+// Entry point function
+////////////////////////////////////////////////////
+int main()
+{
+    char ch ='\0';
+    bool bRet = false;
+
+    printf("Enter character: ");
+    scanf("%c",&ch);
+
+    bRet = CheckCapital(ch);
+
+    if(ch == true)
+    {
+        printf("%c is capital letter\n",ch);
+    }
+    else
+    {
+        printf("%c is not capital letter.\n",ch);
+    }
+
+    return 0;
+}
