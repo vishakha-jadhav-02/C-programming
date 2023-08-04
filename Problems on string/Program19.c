76188aa113ae311d95c8061ccf6821ca18b0f167
Vishakha Krishnarao Jadhav <132731497+vishakha-jadhav-02@users.noreply.github.com>
2023-08-04 20:25:56 +0530

    Create Program19.c
    Accept string from user and replace capital letter with * and small with $

---
+++ Problems on string/Program19.c
@@ -0,0 +1,46 @@
+// Accept string from user and replace capital letter with * and small with $
+
+#include<stdio.h>
+
+/////////////////////////////////////////////////////
+//
+// Function Name:   EditString
+// Input:           String
+// Output:          None
+// Description:     display updated string
+// Author:          Vishakha Krishnarao Jadhav
+// Date:            04/08/23
+//
+////////////////////////////////////////////////////
+void EditString(char *str)
+{
+    while(*str != '\0')
+    {
+        if((*str >= 'A')&& (*str <= 'Z'))
+        {
+            *str ='*';
+        }
+        else if((*str >= 'a')&& (*str <= 'z'))
+        {
+            *str ='$';
+        }
+        str++;
+    }
+}
+
+////////////////////////////////////////////////////
+// Entry point function
+////////////////////////////////////////////////////
+int main()
+{
+    char Arr[30];
+
+    printf("Enter string: ");
+    scanf("%[^\n']s",Arr);
+
+    EditString(Arr);
+
+    printf("String after editing is: %s\n",Arr);
+
+    return 0;
+}
